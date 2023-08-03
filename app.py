c07cb962a4ef615e9e18c524146ec49f3205f270
Sarthak1306 <sarthak13062001@gmail.com>
2023-08-03 19:39:21 +0530

    Add Agent and Agent Chain

--- app.py
+++ app.py
@@ -139,3 +139,8 @@ if uploaded_file:
                 ),
                 prompt=prompt,
             )
+
+            agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+            agent_chain = AgentExecutor.from_agent_and_tools(
+                agent=agent, tools=tools, verbose=True, memory=st.session_state.memory
+            )
